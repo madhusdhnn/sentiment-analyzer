dc3bb607ddf9a03d77aee8bddaafe50533021b2e
madhusdhnn <madhusudhanan.p@gofrugal.com>
2019-11-17 15:40:35 +0530

    Setup Spring boot service with dependencies

---
+++ src/main/java/com/example/sentimentanalyzer/TwitterSentimentAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sentimentanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterSentimentAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterSentimentAnalyzerApplication.class, args);
+	}
+
+}
